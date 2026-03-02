5145e141743bdeb7c6de413905a39ad690b3fb8e
erlanggardtya14-create <erlanggardtya14@gmail.com>
2026-03-02 07:17:57 +0700

    Add files via upload

---
+++ EDUCO-main/src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import './App.css'
+import bromo from './assets/PEMANDANGAN.JPG'
+import logo from './assets/eduko-removebg-preview.png'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <nav className="navbar">
+        <div className="logo">
+          <img src={logo} alt="Logo eduko" className='Lambang'/>
+        </div>
+        <ul className="nav-links">
+          <li>Home</li>
+          <li>Tentang Kami</li>
+          <li>Program</li>
+          <li>Testimoni</li>
+          <li className='Login'>Login</li>
+          <li className='Sign'>Sign in</li>
+        </ul>
+      </nav>
+      
+     <div className="hero">
+  <div className="hero-content">
+    <h1>
+      Welcome to <span>EDUCO</span>
+    </h1>
+
+    <h2>“Generasi Penerus, Hidupkan Lingkungan”</h2>
+
+    <p>
+      asasas asasasasa asasas asasasa asasas asasas 
+      asasas asasas asasas asasas asasas asasas asasasasa asasas asasasa asasas asasas 
+      asasas asasas asasas asasas asasas
+    </p>
+
+    <div className="hero-buttons">
+      <button className="btn-primary">Pelajari</button>
+      <button className="btn-secondary">Program</button>
+    </div>
+  </div>
+</div>
+        
+      <img src={bromo} alt="Gunung Bromo" className='gambar' />
+      
+    </>
+  )
+}
+
+export default App
