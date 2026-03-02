83208d169eeb4e049a127ea4e51afff1ccff21af
Daffa Aditya <187154992+daffa-aditya-p@users.noreply.github.com>
2026-03-02 07:33:22 +0700

    fix: resolve import and configuration errors

--- EDUCO-main/src/App.jsx
+++ EDUCO-main/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import bromo from './assets/PEMANDANGAN.JPG'
+import bromo from './assets/PEMANDANGAN.jpg'
 import logo from './assets/eduko-removebg-preview.png'
 
 
