0f4f9c8264f92187a2163cc4970fa4db310b5546
Athrysd <221656567+Athrysd@users.noreply.github.com>
2026-03-07 22:22:36 +0700

    modified:   EDUCO-main/package-lock.json modified:   EDUCO-main/package.json modified:   EDUCO-main/src/Components/FAQ.jsx modified:   EDUCO-main/src/Components/SliderProgram.jsx modified:   EDUCO-main/src/Routes/TestingComponent.jsx modified:   EDUCO-main/src/index.css

---
+++ EDUCO-main/src/Components/SliderProgram.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Gambar1 from '../../public/Program_Vector_1.svg';
+import Gambar2 from '../../public/Program_Vector_2.svg';
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+const SliderProgram = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+    
+    return (
+      <div className="max-w-md mx-auto px-4">
+      <Slider {...settings}>
+    <div className="px-2">
+      <div className="bg-[#F5C518] rounded-2xl min-h-75 p-5 flex flex-col items-center gap-10">
+        <h1 className="font-bold text-white text-center">Ruang Kelas Pembelajaran Lingkungan</h1>
+        <div className="w-60 mx-auto">
+            <img src={Gambar1} alt="Program Vector 1" />
+        </div>
+      </div>
+    </div>
+    <div className="px-2">
+      <div className="bg-[#F5C518] rounded-2xl min-h-75 p-5 flex flex-col items-center gap-10">
+        <h1 className="font-bold text-white text-center">Game Edukatif Bertema Lingkungan</h1>
+        <div className="w-40 mx-auto">
+            <img src={Gambar2} alt="Program Vector 2" />
+        </div>
+      </div>
+    </div>
+      </Slider>
+      </div>
+  )
+}
+
+export default SliderProgram
--- EDUCO-main/src/Routes/TestingComponent.jsx
+++ EDUCO-main/src/Routes/TestingComponent.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import FAQ from '../Components/FAQ'
+import SliderProgram from '../Components/SliderProgram'
 
 const TestingComponent = () => {
   return (
-    <div>TestingComponent</div>
+    <>
+      <div className='text-center'>Testing Componeng
+      </div>
+
+      {/* FAQ Components*/}
+      <h1>FAQ - Landing Page</h1>
+      <FAQ />
+
+      {/* Slider Components*/}
+      <div className="">
+
+        <h1>Slider - Landing Page</h1>
+        <SliderProgram />
+      </div>
+
+
+      {/* Statistik Components */}
+      
+      <h1>Statistik</h1>
+    </>
   )
 }
 
