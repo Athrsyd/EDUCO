4dce57a83e6f242aeb11e1b0574bbf90857cabd0
Athrysd <221656567+Athrysd@users.noreply.github.com>
2026-03-14 13:20:43 +0700

    new file:   src/assets/Data/Mode Siswa/dataTabel.js

---
+++ src/assets/Data/Mode Siswa/dataTabel.js
@@ -0,0 +1,60 @@
+const dataTabel = [
+    {
+        id: 1,
+        namaSiswa: "John Doe",
+        namaSekolah: "SMP Negeri 1",
+        point: 1046
+    },
+    {
+        id: 2,
+        namaSiswa: "John Doe",
+        namaSekolah: "SMP Negeri 1",
+        point: 1036
+    },
+    {
+        id: 3,
+        namaSiswa: "John Doe",
+        namaSekolah: "SMP Negeri 1",
+        point: 1026
+    },
+    {
+        id: 4,
+        namaSiswa: "John Doe",
+        namaSekolah: "SMP Negeri 1",
+        point: 1016
+    },
+    {
+        id: 5,
+        namaSiswa: "John Doe",
+        namaSekolah: "SMP Negeri 1",
+        point: 1006
+    },
+    {
+        id: 6,
+        namaSiswa: "John Doe",
+        namaSekolah: "SMP Negeri 1",
+        point: 946
+    },
+    {
+        id: 7,
+        namaSiswa: "John Doe",
+        namaSekolah: "SMP Negeri 1",
+        point: 846
+    },
+]
+const dataTopTier = [
+    {
+        id:1,
+        data: dataTabel[0]
+    },
+    {
+        id:2,
+        data: dataTabel[1]
+    },
+    {
+        id:3,
+        data: dataTabel[2]
+    },
+]
+
+export {dataTabel, dataTopTier}
